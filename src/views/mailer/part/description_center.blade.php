a90ba668b24b256db9bc3bee5eceae53ad8bf88d
arturishe21 <artur@vis-design.com>
2015-08-03 23:25:26 +0300

    add translata mailer box

--- src/views/mailer/part/description_center.blade.php
+++ src/views/mailer/part/description_center.blade.php
@@ -1,26 +1,26 @@
 <div class="modal-header">
     <button type="button" class="close" data-dismiss="modal" aria-hidden="true"> &times; </button>
-    <h4 class="modal-title" id="modal_form_label">{{__cms('Письмо ')}} №{{$mail->id}} </h4>
+    <h4 class="modal-title" id="modal_form_label">{{__cms('Письмо')}} №{{$mail->id}} </h4>
 </div>
 <div class="modal-body">
     <section>
         <p>
-            <strong>Тема письма</strong>: {{$mail->subject}}
+            <strong>{{__cms('Тема письма')}}</strong>: {{$mail->subject}}
         </p>
     </section>
 
     <section>
         <p>
-            <strong>Кому</strong>: {{$mail->email_to}}
+            <strong>{{__cms('Кому')}}</strong>: {{$mail->email_to}}
         </p>
     </section>
     <section>
         <p>
-            <strong>Дата отправки</strong>: {{$mail->created_at}}
+            <strong>{{__cms('Дата отправки')}}</strong>: {{$mail->created_at}}
         </p>
     </section>
     <section>
-        <p><strong>Тело письма</strong>:</p>
+        <p><strong>{{__cms('Тело письма')}}</strong>:</p>
         <div>
            {{$mail->body}}
         </div>
